15215132231dfe1670f1be14a0a9ef8a233a81ab
Rafael Batista <121529830+RafaBatista21@users.noreply.github.com>
2023-01-08 23:20:19 -0300

    Add files via upload

--- Primeiro Projeto (Mentoria)/projeto1.js
+++ Primeiro Projeto (Mentoria)/projeto1.js
@@ -7,31 +7,32 @@ const numRand= Math.floor(Math.random() * (20 + 1));
 enviarButton.addEventListener("click", function(e){
 
     const valorUser= valorDigitado.value;
-    console.log(valorUser);
-
-    console.log(numRand);
 
 
-    if(numRand==valorUser){
+    if (valorUser<=20){
+        if(numRand==valorUser){
 
-        resposta.textContent= "VOCE ACERTOU";
-    }
-    else if ( numRand>valorUser)
-    {
-        resposta.textContent = "Errou. Um conselho o numero é maior que esse."
+            resposta.style.color= "green"; 
+            resposta.textContent= "VOCE ACERTOU";
+        }
+        else if ( numRand>valorUser)
+        {
+            resposta.style.color= "red";
+            resposta.textContent = "Errou. Um conselho o numero é MAIOR que esse."
 
-    }
-    else if (numRand<valorUser){
+        }
+        else if (numRand<valorUser){
 
-        resposta.textContent = "Errou. Um conselho o numero é menor que esse." 
+            resposta.style.color= "red";
+            resposta.textContent = "Errou. Um conselho o numero é MENOR que esse." 
+
+        }
 
     }
     else{
-
-        resposta.textContent = "Voce digitou um valor fora dos limites de 1 a 20." 
-    }    
-
-
+        resposta.style.color= "white";
+        resposta.textContent = "Voce digitou um valor fora dos limites de 0 a 20." 
+    }
 
 
 });
